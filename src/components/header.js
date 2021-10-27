1a1e807fd2c86c7ee78721d2ba3be31d672646f9
mateoroldos <mateoroldos19@gmail.com>
2021-10-27 10:28:39 -0300

    feat: :sparkles: Basic form funtionality

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,34 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import {
+  Flex,
+  Spacer,
+  Link,
+  Center,
+  HStack,
+  StackDivider,
+} from "@chakra-ui/react"
+import { Link as GatsbyLink } from "gatsby"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <Flex direction="row" as="header" h="10vh" w="100%" pos="fixed">
+    <Center>
+      <Link as={GatsbyLink} to="/">
+        {siteTitle}
+      </Link>
+    </Center>
+
+    <Spacer />
+
+    <HStack spacing={3} divider={<StackDivider />} as="nav">
+      <Link as={GatsbyLink} to="/">
+        Home
+      </Link>
+      <Link as={GatsbyLink} to="/page-2">
+        Page 2
+      </Link>
+    </HStack>
+  </Flex>
 )
 
 Header.propTypes = {
