c79f93178fef2b9e1b7034c2dfbbd1167b61345f
mateoroldos <mateoroldos19@gmail.com>
2021-11-17 17:36:56 -0300

    fix: :bug: Folders names uppercase fix

--- src/components/Form/Inputs/AirportInput.jsx
+++ src/components/Form/Inputs/AirportInput.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 
-import { airports } from "../../../constantss/airports"
+import { airports } from "../../../constants/airports"
 
 import { Image, Select, Center, Button } from "@chakra-ui/react"
 
