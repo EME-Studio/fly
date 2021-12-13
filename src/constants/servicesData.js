9cc66bf745fd4011d3c0acff6a82d06451ab0a6e
mateoroldos <mateoroldos19@gmail.com>
2021-12-13 13:19:03 -0300

    fix: :bug: Zoom on iphone bug

--- src/constants/servicesData.js
+++ src/constants/servicesData.js
@@ -1,6 +1,11 @@
 import Servicios1 from "../images/servicios-1.jpeg"
+import Servicios2 from "../images/servicios-2.jpeg"
+import Servicios3 from "../images/servicios-3.jpeg"
 
 const servicios1 = Servicios1
+const servicios2 = Servicios2
+const servicios3 = Servicios3
+const servicios4 = Servicios1
 
 export const servicesData = [
   {
@@ -10,19 +15,19 @@ export const servicesData = [
       "La flota más moderna de aeronaves para que tengas una experiencia de primer nivel en vuelos privados, sin importar el destino que elijas Los más estrictos estándares de seguridad con el confort que solo FLY te puede ofrecer. Con FLY viajar es mucho más que volar.",
   },
   {
-    image: servicios1,
+    image: servicios2,
     title: "Vuelos charter",
     content:
       "La flota más moderna de aeronaves para que tengas una experiencia de primer nivel en vuelos privados, sin importar el destino que elijas Los más estrictos estándares de seguridad con el confort que solo FLY te puede ofrecer. Con FLY viajar es mucho más que volar.",
   },
   {
-    image: servicios1,
+    image: servicios3,
     title: "Empty leg",
     content:
       "La flota más moderna de aeronaves para que tengas una experiencia de primer nivel en vuelos privados, sin importar el destino que elijas Los más estrictos estándares de seguridad con el confort que solo FLY te puede ofrecer. Con FLY viajar es mucho más que volar.",
   },
   {
-    image: servicios1,
+    image: servicios4,
     title: "FBO",
     content:
       "La flota más moderna de aeronaves para que tengas una experiencia de primer nivel en vuelos privados, sin importar el destino que elijas Los más estrictos estándares de seguridad con el confort que solo FLY te puede ofrecer. Con FLY viajar es mucho más que volar.",
