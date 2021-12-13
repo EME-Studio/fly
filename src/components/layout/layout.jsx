d00dbe1fe94529896b06dfa4bc692dd4e6f789ef
mateoroldos <mateoroldos19@gmail.com>
2021-12-13 13:28:31 -0300

    fix: :bug: Iphone zoom fix

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -8,6 +8,7 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { Helmet } from "react-helmet"
 
 import Header from "./header"
 import "./layout.css"
@@ -26,6 +27,13 @@ const Layout = ({ children, headerRojo }) => {
 
   return (
     <Box minH="100vh" h="1px" display="flex" flexDirection="column">
+      {/* iphone zooming fix */}
+      <Helmet>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+      </Helmet>
       <Header
         conColor={headerRojo ? true : false}
         siteTitle={data.site.siteMetadata?.title || `Title`}
