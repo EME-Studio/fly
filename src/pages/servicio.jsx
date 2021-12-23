cfaee62517cfb84e88b3dd329f069601cb52c957
mateoroldos <mateoroldos19@gmail.com>
2021-12-23 13:53:17 -0300

    fix: :bug: Services pagination margin fix

--- src/pages/servicio.jsx
+++ src/pages/servicio.jsx
@@ -27,7 +27,7 @@ const Servicio = () => (
           ".splide__arrow svg": { fill: "fly.main" },
           ".splide__arrow svg:hover": { fill: "fly.main" },
           ".splide__pagination__page.is-active": { background: "fly.main" },
-          ".splide__pagination": { position: "absolute", bottom: "0px" },
+          ".splide__pagination": { position: "static", marginTop: "15px" },
           ".splide__track": { paddingY: "20px" },
           "@media only screen and (max-width: 48em)": {
             ".splide__arrow": { display: "none" },
